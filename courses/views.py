d4bcc781169cbc5e95d840bdc26443e894a052bc
Oghene <officialoghene@gmail.com>
2020-08-21 18:06:49 +0100

    Views, forms, url, template and templatetags

--- courses/views.py
+++ courses/views.py
@@ -7,7 +7,7 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
 from django.contrib.auth.decorators import login_required
-from .models import Course, Module
+from .models import Course, Module, Content
 from .forms import ModuleFormSet
 
 
@@ -86,24 +86,77 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
             return redirect('manage_course_list')
         return self.render_to_response({'course': self.course, 'formset': formset})
 
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = Content
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+    
+    def get_model(self, model_name):
+        if model_name == Content:
+            return apps.get_model(app_label='courses',
+                                    model_name=Content)
+        return None
+    
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(model, exclude=['owner','created'])
+        return Form(*args, **kwargs)
+    
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(Module,
+                                        id=module_id,
+                                        course__owner=request.user)
+        self.model = Content
+        if id:
+            self.obj = get_object_or_404(self.model,
+                                            id=id,
+                                            owner=request.user)
+        return super().dispatch(request, module_id, model_name, id)
+    
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form, 'object': self.obj})
+        
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model,
+        instance=self.obj,data=request.POST,files=request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+            # new content
+                Content.objects.create(module=self.module,item=obj)
+            return redirect('module_content_list', self.module.id)
+        return self.render_to_response({'form': form,'object': self.obj})
 
 
-@login_required
-def edit(request):
-    if request.method == "POST":
-        user_form = UserEditForm(instance=request.user, data=request.POST)
 
-        profile_form = ProfileEditForm(
-            instance=request.user.profile, data=request.POST, files=request.FILES)
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+    def get(self, request, module_id):
+        module = get_object_or_404(Module,
+                                    id=module_id,
+                                    course__owner=request.user)
+        return self.render_to_response({'module': module})
 
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, "Profile updated successfully")
-        else:
-            messages.error(request, 'Error updating your profile')
-    else:
-        user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user.profile)
 
-    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
+# @login_required
+# def edit(request):
+#     if request.method == "POST":
+#         user_form = UserEditForm(instance=request.user, data=request.POST)
+
+#         profile_form = ProfileEditForm(
+#             instance=request.user.profile, data=request.POST, files=request.FILES)
+
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user_form.save()
+#             profile_form.save()
+#             messages.success(request, "Profile updated successfully")
+#         else:
+#             messages.error(request, 'Error updating your profile')
+#     else:
+#         user_form = UserEditForm(instance=request.user)
+#         profile_form = ProfileEditForm(instance=request.user.profile)
+
+#     return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
